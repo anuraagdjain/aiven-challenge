9d8b596e65064b9da57eeddc5792449bc38a8c6e
Anuraag Jain <thehray@gmail.com>
2022-08-13 23:50:32 +0300

    feat(producer): publish website data onto broker

--- src/producer/healthcheck.py
+++ src/producer/healthcheck.py
@@ -1,15 +1,22 @@
+import logging
 import requests
 
 class HealthCheck:
-    def __init__(self, website_urls = []):
+    def __init__(self,publisher, website_urls = []):
         self.website_urls = website_urls
+        self.publisher = publisher
+        self.topic =  "services-web-health"
+
     def run(self):
-        result = []
         for website_url in self.website_urls:
+            logging.info('[HealthCheck.run] Checking page {}'.format(website_url))
             response = requests.get(website_url)
             payload = dict()
             payload["url"] = website_url
             payload["time_in_ms"] = response.elapsed.total_seconds() * 1000
             payload["http_status_code"] = response.status_code
-            result.append(payload)
-        return result
+            self.publish(payload)
+    
+    def publish(self, payload):
+        logging.info("[HealthCheck.publish] Publishing payload for {}".format(payload["url"]))
+        self.publisher.send(self.topic, payload)
--- tests/unit/healthcheck.py
+++ tests/unit/healthcheck.py
@@ -1,11 +1,20 @@
-from src.producer.healthcheck import HealthCheck
 import responses
 import pytest
+from unittest.mock import MagicMock, patch
+from src.producer.healthcheck import HealthCheck
+
 
 @pytest.fixture
-def healthcheck_instance():
+@patch('kafka.KafkaProducer', MagicMock())
+@patch('src.producer.publisher.MessagePublisher')
+def message_publisher(messagePublisher):
+    return messagePublisher
+
+@pytest.fixture
+@pytest.mark.usefixtures("message_publisher")
+def healthcheck_instance(message_publisher):
     website_urls = ["https://google.com","https://aiven.io"]
-    return HealthCheck(website_urls)
+    return HealthCheck(message_publisher, website_urls)
 
 @pytest.fixture
 def setup_mock_response():
@@ -21,25 +30,21 @@ def setup_mock_response():
         status=502))
     
 
+
 @pytest.mark.usefixtures("healthcheck_instance")
 @pytest.mark.usefixtures("setup_mock_response")
+@pytest.mark.usefixtures("message_publisher")
 class TestHealthCheck:
     @responses.activate
-    def test_run_returns_payload_in_expected_format(self, healthcheck_instance):
+    def test_publishes_on_broker(self, healthcheck_instance, message_publisher):
+        healthcheck_instance.run()
         
-
-        result = healthcheck_instance.run()
-        
-        assert isinstance(result, list)
-        for data in result:
-            assert ["url","time_in_ms","http_status_code"] == list(data.keys())
-            assert isinstance(data["url"], str)
-            assert isinstance(data["http_status_code"], int)
-            assert isinstance(data["time_in_ms"], float)
+        assert message_publisher.send.called
+        assert message_publisher.send.call_count == 2
 
 
     @responses.activate
-    def test_payload_has_expected_data(self, healthcheck_instance):
+    def test_payload_has_expected_data(self, healthcheck_instance, message_publisher):
         responses.add(responses.Response(
         method="GET",
         url="https://google.com",
@@ -51,10 +56,18 @@ class TestHealthCheck:
         url="https://aiven.io",
         status=502))
 
-        result = healthcheck_instance.run()
+        healthcheck_instance.run()
+
+        call_args = message_publisher.send.call_args_list
+
+        first_call = call_args[0].args
+        second_call = call_args[1].args
         
-        assert result[0]["url"] == "https://google.com"
-        assert result[0]["http_status_code"] == 404
-        assert result[1]["url"] == "https://aiven.io"
-        assert result[1]["http_status_code"] == 502
+        assert first_call[0] == 'services-web-health'
+        assert first_call[1]["url"] == "https://google.com"
+        assert first_call[1]["http_status_code"] == 404
+        
+        assert second_call[0] == 'services-web-health'
+        assert second_call[1]["url"] == "https://aiven.io"
+        assert second_call[1]["http_status_code"] == 502
 
